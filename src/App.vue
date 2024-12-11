8dff36ad6f14a2ab8021584d80b7afe2876d01c7
Bank <blankinfear@gmail.com>
2024-12-11 14:56:54 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <img v-bind:src="img" :width="size" alt="">
+    <img :src="img" :width="size" alt="">
     <h1>name : {{ getFullname() }}</h1>
+    <p>age : {{ age }}</p>
     <p>address : <span v-html="address"></span></p>
     <ul>
       <li v-for="(item, index) in hobby" :key="index">{{ item }}</li>
     </ul>
+    <p>infomation</p>
+    <ul>
+      <li v-for="([key, value], idx) in Object.entries(general)" :key="idx">
+        {{ key }}: {{ value }}
+      </li>
+    </ul>
+    <button @click="showData">Click</button>
+    <button @click="increment">ADD</button>
+    <button @click="decrement">DROP</button>
   </div>
 </template>
 
@@ -16,15 +26,26 @@ export default {
     return {
       firstname: "Bank",
       lastname: "Mergency",
+      age: 30,
       address: "<strong>Donmuang</strong>",
       img: "https://science.nasa.gov/wp-content/uploads/2023/05/sun-cartoon-crop.png?w=4096&format=png&crop=1",
       size: 200,
-      hobby: ["play a game", "travel", "sleep"]
+      hobby: ["play a game", "travel", "sleep"],
+      general: {gender : "male", weight: 75, height: 180},
     };
   },
   methods: {
     getFullname() {
       return `${this.firstname} ${this.lastname}`;
+    },
+    showData(){
+      alert(this.getFullname())
+    },
+    increment(){
+      this.age++
+    },
+    decrement(){
+      this.age--
     }
   }
 };
